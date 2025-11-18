f5dedfc585036796137d1227600d94b5b7ee857f
Adil Akhmetov <adilahmetov2323@gmail.com>
2025-11-18 07:58:00 +0500

    Первый коммит: добавил CreateBot и структуру проекта

---
+++ Bots/CreateBot.cs
@@ -0,0 +1,11 @@
+using BooHarikCreateBot;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string token = "ТОКЕН";
+        var bot = new CreateBot(token);
+        await bot.StartAsync();
+    }
+}
